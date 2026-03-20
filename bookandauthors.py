0b821ed741e8a198d2a2473cfc88097eff162346
Karen Comandante <karencomandante19@gmail.com>
2026-03-20 20:09:50 +0800

    Refactor Book and Author classes with object creation

--- bookandauthors.py
+++ bookandauthors.py
@@ -5,7 +5,6 @@ class Book:
         self._author = author 
         self._pages = pages  
 
-
     def display_info(self):  # Abstraction: Exposes only necessary features to interact with the class
         print(f"Title: {self._title}")  
         print(f"Author: {self._author}")  
