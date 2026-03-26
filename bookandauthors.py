8590d56c9aae9e17cc9e16f5d3133709c897211f
Karen Comandante <karencomandante19@gmail.com>
2026-03-26 19:26:04 +0800

    Refactor Book and Author classes with examples

--- bookandauthors.py
+++ bookandauthors.py
@@ -41,7 +41,7 @@ fiction_books = [
     ("And Then There Were None", "Agatha Christie", 200, "Mystery"),
     ("Harry Potter and the Philosopher's Stone", "J.K. Rowling", 320, "Fantasy") ] 
 
-nonfiction_info = ("Sapiens", "Yuval Noah Harari", 443, "History") 
+nonfiction_info = ("Sapiens", "Yuval Noah Harari", 443, "History")  
 
 # Create objects and call methods
 for info in fiction_books:
