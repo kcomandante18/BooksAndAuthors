cc28575303df8e072f7b2423d0550d1fee415186
Karen Comandante <karencomandante19@gmail.com>
2026-04-09 18:27:06 +0800

    Update bookandauthors.py

--- bookandauthors.py
+++ bookandauthors.py
@@ -1,6 +1,6 @@
 
 class Book:
-    def __init__(self, title, author, pages):  # Encapsulation: Constructor to initialize object state.  
+    def __init__(self, title, author, pages):  # Encapsulation: Constructor to initialize object state.   
         self._title = title  
         self._author = author  
         self._pages = pages  
